770d5df868db89248040bb9cc286c51c857a50f0
David Jacot <david.jacot@gmail.com>
2013-04-10 10:58:25 +0200

    use sr org

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aleph "0.2.1-rc4"
+(defproject org.clojars.smallrivers/aleph "0.2.1-rc4"
   :description "a framework for asynchronous communication"
   :repositories {"jboss" "http://repository.jboss.org/nexus/content/groups/public/"
                  "sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
